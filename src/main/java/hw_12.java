c4272eb5a7993bc11b782662afc5f0c6e2154333
VladPt <m5work10@gmail.com>
2021-12-14 16:14:43 +0200

    deleted unnecessary imports

--- src/main/java/hw_12.java
+++ src/main/java/hw_12.java
@@ -1,25 +1,5 @@
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import org.json.simple.*;
-import java.io.FileWriter;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.NoSuchElementException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
-import org.apache.http.HttpHeaders;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 
 public class hw_12 {
