fe2909f09a6db4d1e19bb806465aea8ac77d0927
VladPt <m5work10@gmail.com>
2021-12-14 17:00:55 +0200

    task 2 added

--- src/main/java/hw_12.java
+++ src/main/java/hw_12.java
@@ -5,10 +5,11 @@ import java.util.concurrent.Executors;
 public class hw_12 {
 
     public static void main(String[] args) {
-        /* try {
+        try {
+            int moleculeAmount = 3
             ExecutorService threadPool = Executors.newFixedThreadPool(3);
             H2OGenerator GeneratorH2O = new H2OGenerator();
-            for(int i = 0; i < 5; i++) {
+            for(int i = 0; i < moleculeAmount; i++) {
                 threadPool.execute(() -> GeneratorH2O.oxygenReleaser(new ReleaseOxygen()));
                 threadPool.execute(() -> GeneratorH2O.hydrogenReleaser(new ReleaseHydrogen()));
                 threadPool.execute(() -> GeneratorH2O.hydrogenReleaser(new ReleaseHydrogen()));
@@ -16,7 +17,7 @@ public class hw_12 {
             threadPool.shutdown();
         } catch (Exception ex) {
             ex.printStackTrace();
-        }*/
+        }
         //task 2
         CustomThreadPoolExecutor customThreadPoolExecutor =
                 new CustomThreadPoolExecutor(10);
