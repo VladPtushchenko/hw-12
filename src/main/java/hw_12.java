b87d1567a261c4d5a8f4a20df455aa00e1d455cb
VladPt <m5work10@gmail.com>
2021-12-14 14:19:08 +0200

    clean project

---
+++ src/main/java/hw_12.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import org.json.simple.*;
+import java.io.FileWriter;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.NoSuchElementException;
+
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+public class hw_12 {
+
+    public static void main(String[] args) {
+        try {
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+}
